a502cf6fc5e6bca73ad5324df3822ed82b77bf81
TJAM\erick.bolorini <erick.bolorini@tjam.jus.br>
2023-06-27 09:06:55 -0400

    Correções no filtro por outros campos

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -73,28 +73,29 @@ export class UserListComponent implements OnInit {
   }
 
   filterByAnyField(user: any, filterValue: string): boolean {
-    const fieldsToFilter = ['name', 'email', 'address', 'company'];   // Campos nos quais será realizado o filtro
+    // Filtra pelos outros campos
+    const fieldsToFilter = ['name', 'email', 'address', 'company']; 
     for (const key of fieldsToFilter) {
       if (
-        user.hasOwnProperty(key) &&                                // Verifica se o usuário possui o campo atual
-        typeof user[key] === 'string' &&                            // Verifica se o valor do campo é uma string
-        user[key].toLowerCase().includes(filterValue)               // Verifica se o valor do campo contém o valor do filtro, ignorando maiúsculas e minúsculas
+        user.hasOwnProperty(key) && 
+        typeof user[key] === 'string' && 
+        user[key].toLowerCase().includes(filterValue) 
       ) {
         return true;
       } else if (
         key === 'address' &&
-        user.hasOwnProperty('address') &&                           // Verifica se o usuário possui o campo 'address'
-        user.address.hasOwnProperty('city') &&                      // Verifica se o campo 'address' possui o campo 'city'
-        typeof user.address.city === 'string' &&                     // Verifica se o valor de 'city' é uma string
-        user.address.city.toLowerCase().includes(filterValue)        // Verifica se o valor de 'city' contém o valor do filtro, ignorando maiúsculas e minúsculas
+        user.hasOwnProperty('address') && 
+        user.address.hasOwnProperty('city') &&  
+        typeof user.address.city === 'string' && 
+        user.address.city.toLowerCase().includes(filterValue)
       ) {
         return true;
       } else if (
         key === 'company' &&
-        user.hasOwnProperty('company') &&                           // Verifica se o usuário possui o campo 'company'
-        user.company.hasOwnProperty('name') &&                      // Verifica se o campo 'company' possui o campo 'name'
-        typeof user.company.name === 'string' &&                     // Verifica se o valor de 'name' é uma string
-        user.company.name.toLowerCase().includes(filterValue)        // Verifica se o valor de 'name' contém o valor do filtro, ignorando maiúsculas e minúsculas
+        user.hasOwnProperty('company') && 
+        user.company.hasOwnProperty('name') && 
+        typeof user.company.name === 'string' &&  
+        user.company.name.toLowerCase().includes(filterValue) 
       ) {
         return true;
       }
